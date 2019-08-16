c109ce694963e5477c870dfef793514b17bc70f3
christian <13sivwrchr@gmail.com>
2019-08-16 18:08:36 +0800

    firebase test signup form

--- backendDemo/pages/index.vue
+++ backendDemo/pages/index.vue
@@ -1,53 +1,44 @@
 <template>
-  <div class="container">
-    <form name="contact" action method="post">
-      <label class="form-label" for="user">User:</label>
-      <input class="form-field" name="user" id="user" />
-
-      <label class="form-label" for="password">Password:</label>
-      <textarea class="form-field" name="password" id="password"></textarea>
-      <input class="form-button" type="submit" value="Send message" />
-    </form>
+  <div class="admin-new-post-page">
+    <section class="new-post-form">
+      <AdminPostForm @submit="onSubmitted" />
+    </section>
   </div>
 </template>
 
 <script>
-import Logo from "~/components/Logo.vue";
+import axios from "axios";
+import AdminPostForm from "@/components/AdminPostForm";
 
 export default {
-  components: {}
+  layout: "admin",
+  components: {
+    AdminPostForm
+  },
+  methods: {
+    onSubmitted(postData) {
+      axios
+        .post(
+          "https://team-14-ontologies.firebaseio.com/NewUser.json",
+          postData
+        )
+        .then(result => console.log(result))
+        .catch(e => console.log(e));
+    }
+  }
 };
 </script>
 
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
+<style scoped>
+.new-post-form {
+  width: 90%;
+  margin: 20px auto;
 }
 
-.title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
-    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+@media (min-width: 768px) {
+  .new-post-form {
+    width: 500px;
+  }
 }
 </style>
+
