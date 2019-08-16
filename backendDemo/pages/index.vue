34282471fe3ea6170d1ed4c34f0bfd2893f14633
ishasingh2 <ishasingh@berkeley.edu>
2019-08-16 18:15:41 +0800

    t
    t

--- backendDemo/pages/index.vue
+++ backendDemo/pages/index.vue
@@ -1,14 +1,7 @@
+
 <template>
   <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">backendDemo</h1>
-      <h2 class="subtitle">My priceless Nuxt.js project</h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
-        <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>
-      </div>
-    </div>
+    <form/>
   </div>
 </template>
 
---
+++ backendDemo/store/index.js
@@ -0,0 +1,30 @@
+import Vuex from "vuex";
+import axios from "axios";
+
+const createStore = () => {
+  return new Vuex.Store({
+    state: {
+      loadedPosts: []
+    },
+    mutations: {
+      setPosts(state, posts) {
+        state.loadedPosts = posts;
+      }
+    },
+    actions: {
+      nuxtServerInit(vuexContext, context) {
+        return axios.get();
+      },
+      setPosts(vuexContext, posts) {
+        vuexContext.commit("setPosts", posts);
+      }
+    },
+    getters: {
+      loadedPosts(state) {
+        return state.loadedPosts;
+      }
+    }
+  });
+};
+
+export default createStore;
