8d5ab5abd19831ab7a3b454367994352ca1f0374
Qwazyoxyphi <39331035+Qwazyoxyphi@users.noreply.github.com>
2019-08-17 14:01:19 +0800

    Project upload test

---
+++ backendDemo/components/ProjectCreation.vue
@@ -0,0 +1,48 @@
+<template>
+    <form @submit.prevent="onSubmit">
+    <AppControlInput v-model="projectCreate.projname" >ProjectName:</AppControlInput>
+
+    <AppButton type="submit">Create</AppButton>
+    <AppButton type="button" style="margin-left: 10px" btn-style="cancel" @click="onCancel">Cancel</AppButton>
+  </form>
+</template>
+
+<script>
+import AppControlInput from "@/components/UI/AppControlInput";
+import AppButton from "@/components/UI/AppButton";
+
+export default {
+  components: {
+    AppControlInput,
+    AppButton
+  },
+  props: {
+    post: {
+      type: Object,
+      required: false
+    }
+  },
+  data() {
+    return {
+      projectCreate: this.post
+        ? { ...this.post }
+        : {
+            projname: "",
+            dateCreated: new Date().toLocaleDateString(),
+            owner: "u1",
+            members: ""
+          }
+    };
+  },
+    methods:{
+        onSubmit(){
+           this.$emit("submit", this.projectCreate);
+        },
+        onCancel() {
+            // Navigate back
+            this.$router.push("/");
+        }
+    }
+    
+};
+</script>
