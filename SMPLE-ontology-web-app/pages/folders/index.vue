4748c7e19eee5759c5bf035fc2a2e305b75effba
Jason Veljanoski <Jason@jason-2.local>
2019-08-27 08:43:39 +0800

    folder page skeleton

--- SMPLE-ontology-web-app/pages/folders/index.vue
+++ SMPLE-ontology-web-app/pages/folders/index.vue
@@ -1,5 +1,25 @@
 <template>
   <div>
-    <h1>This is the folders route</h1>
+    <AppSection
+      title="This is the folders route"
+      color="blue"
+    >
+      <p>this is text</p> 
+    </AppSection>
   </div>
 </template>
+
+<script>
+import AppSection from '@/components/Utilities/AppSection'
+import AppSubtitle from '@/components/Utilities/AppSubtitle'
+
+export default {
+  components: {
+    AppSubtitle,
+    AppSection
+  },
+  data() {
+    return {}
+  }
+}
+</script>
