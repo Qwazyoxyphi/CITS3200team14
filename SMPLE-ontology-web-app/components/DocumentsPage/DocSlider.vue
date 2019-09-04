bb810aa05a350cd02816a8409e2d875360e1bed6
Mentallistic <mitchel.p97@live.com.au>
2019-09-04 14:56:00 +0800

    Homepage Text & Slider
    Added some homepage text. Feel free to edit it if need be.
    
    Also added slider component. Currently still being debugged. Issues currently revolve around refreshing documents page, causing the page to crash. I may need help resolving this issue...

---
+++ SMPLE-ontology-web-app/components/DocumentsPage/DocSlider.vue
@@ -0,0 +1,37 @@
+<template>
+  <no-ssr>
+    <vueper-slides 
+      :visible-slides="3"  
+      :slide-ratio="1/4" 
+      :dragging-distance="200" 
+      :breakpoints="{ 800: { visibleSlides: 2 } }"
+      slide-multiple
+    >
+      <vueper-slide
+        v-for="document in documents" 
+        :key="document.id" 
+        :title="document.documentName" 
+        :content="document.documentDesc"
+        link:=""
+      />
+    </vueper-slides>
+  </no-ssr>
+</template>
+
+<script>
+import { VueperSlides, VueperSlide } from 'vueperslides'
+import 'vueperslides/dist/vueperslides.css'
+
+export default {
+  components: {
+    VueperSlides,
+    VueperSlide
+  },
+  props: {
+    documents: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
--- SMPLE-ontology-web-app/pages/index.vue
+++ SMPLE-ontology-web-app/pages/index.vue
@@ -1,16 +1,39 @@
 <template>
   <div>
-    <AppSection
-      title="hello"
+    <AppSection 
+      title="Welcome to Ontology Visualisations" 
       color="blue"
     >
-
+      
       <AppModal :mini-player="true">
-        <AppVideo
-          video-id="204135196"
-          color="green"
+        <AppVideo 
+          video-id="204135196" 
+          color="green" 
         />
       </AppModal>
+
+      <p class="intro-text">
+        <br>The study of “Ontology” concerns itself with questions relating to the nature of
+        existence, what properties can explain existence, and how these properties explain
+        the differences of existence between existing entities (etc.).
+        <br>
+        <br>This website allows for the creation of graphical ontological representations, consisting
+        of components that visualize the relationships between different “beings” of existence.
+        <br>
+        <br>It can visualize the individual members of certain sets (or classes) of objects, their
+        attributes (the characteristics of those objects), how these objects and classes are
+        related.
+        <br>
+        <br>It can also describe the nature of these relations by allowing for the
+        specification of any function terms relating to these relations, the relations’
+        restrictions, rules that can be logically expressed in these relations, any axioms of the
+        domain of a specific Ontology, and possible events that may affect the relations between
+        different objects and their state of “being”.
+        <br>
+        <br>( Dev Note: This is just some draft text. Changes can easily be made in the future, just be sure
+        to let me know.
+        <br>-Mitch )
+      </p>
 
     </AppSection>
   </div>
