7d07f1902edeef1741da39dde5372b499a5961a6
ishasingh2 <ishasingh@berkeley.edu>
2019-09-10 12:08:35 +0800

    Persisting user

--- SMPLE-ontology-web-app/pages/folders/index.vue
+++ SMPLE-ontology-web-app/pages/folders/index.vue
@@ -23,6 +23,7 @@ import AppSelect from '@/components/FolderPage/More/AppSelect'
 import axios from 'axios'
 
 export default {
+  middleware: ['auth', 'check-auth'],
   /*asyncData(context) { //example for getting folders using uid, same issue as previous> data type issue
   //load folders onto folderpage
     return axios
