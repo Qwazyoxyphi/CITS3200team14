3474b8061da222db07b9c36e2b1757d53dc8b6f5
christian <13sivwrchr@gmail.com>
2019-09-10 16:49:40 +0800

    redirect for testing on this branch

--- SMPLE-ontology-web-app/middleware/auth.js
+++ SMPLE-ontology-web-app/middleware/auth.js
@@ -2,6 +2,6 @@ export default function(context) {
   console.log('[Middleware] Just Auth')
   if (context.store.getters.isAuthenticated == false) {
     console.log('NOT AUTHENTICATED- SHOULD REDIRECT')
-    //context.redirect('/signin-signup')
+    context.redirect('/signin-signup')
   }
 }
