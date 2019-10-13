55413fb630dbb8c62b68454c4a97bd23c892b4ff
Mentallistic <mitchel.p97@live.com.au>
2019-10-13 13:53:11 +0800

    Test Case A complete!
    Created a testing program for case A. Works with firefox (may implement chrome test later).
    
    I've given names to the form-switch button on the sign-up/sign-in page for Selenium to look for.
    
    Clarified aspects of the Selenium install process in Selenium document.
    
    Pushed working ChromeDriver demo to ontologyDemo.

--- Selenium/ontologyDemo.js
+++ Selenium/ontologyDemo.js
@@ -31,4 +31,32 @@ async function demoFirefox() {
   await signOutElement.click();                                     // Signs out the test account
 }
 
+async function demoChrome() {
+  let driver = await new Builder().forBrowser('chrome').build(); // Sets up the chrome browser for automation
+  driver.get('http://localhost:3000/');                           // Tells the browser what domain to visit (localHost for dev)
+
+  let signInElement = await driver.findElement(By.linkText('Sign in / Sign up')); // Finds the sign-in/sign-up button
+  await signInElement.click();                                                    // Clicks the button to get to sign-in page
+
+  await driver.getTitle().then(function (title) { // Looks at page title
+    console.log('Page title is: ' + title);       // Should return the title of the page
+  });
+
+  await driver.wait(until.elementLocated(By.name("email")));                // Waits until the page has loaded and the email element exists
+  let emailElement = await driver.findElement(By.name("email"));            // Finds the email input
+  await emailElement.sendKeys('test@test.com');                             // Inputs the test account email
+
+  let passwordElement = await driver.findElement(By.name("password"));      // Finds the password input. We can assume it exists if the email element exists
+  await passwordElement.sendKeys('tester');                                 // Inputs the test account password
+
+  let submitElement = await driver.findElement(By.id("contact-submit"));    // Finds the submission button. We can assume it exists if the email element exists
+  await submitElement.click();                                              // Submits user details to login, with a click
+
+  await driver.wait(until.elementLocated(By.id("signOut")));                // Waits until the page has loaded and the sign-out element exists
+
+  let signOutElement = await driver.findElement(By.id("signOut"));  // Finds the signOut element (by looking for its identity value)
+  await signOutElement.click();                                     // Signs out the test account
+}
+
 demoFirefox();
+demoChrome();
